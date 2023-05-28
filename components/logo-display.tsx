5cc2870a0cd540cd074df0d2d26e2dd6fd153125
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-05-29 00:51:46 +0200

    chore(landing page): improve (#14)

---
+++ components/logo-display.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { useTheme } from 'next-themes'
+import Image from 'next/image'
+import type { FC } from 'react'
+
+const LogoDisplay: FC = () => (
+  <div className='flex flex-col items-center justify-center'>
+    <Image
+      src={`/temp-logo-${useTheme().theme === 'light' ? 'black' : 'white'}.svg`}
+      alt='Ada URL Parser'
+      width={300}
+      height={300}
+      className='mb-8'
+    />
+  </div>
+)
+
+export default LogoDisplay
