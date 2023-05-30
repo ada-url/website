00eb3e245a829f3656cda54189cfd8b51a448043
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-05-30 23:26:23 +0200

    fix: build

--- components/localizedLink.tsx
+++ components/localizedLink.tsx
@@ -2,15 +2,17 @@
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
 import type { FC, ReactNode } from 'react'
+import type { UrlObject } from 'url'
 
 type Props = {
-  href: string | URL
+  href: string | URL | URLSearchParams | UrlObject
   children: ReactNode
   className?: string
   target?: string
+  rel?: string
 }
 
-const LocalizedLink: FC<Props> = ({ href, children, className }) => {
+const LocalizedLink: FC<Props> = ({ href, children, className, target, rel }) => {
   const params = useParams()
   const path = `/${params.lang}${href}`
 
@@ -18,7 +20,8 @@ const LocalizedLink: FC<Props> = ({ href, children, className }) => {
     <Link
       href={path}
       className={className}
-      target={href.toString().startsWith('http') ? '_blank' : undefined}
+      target={target}
+      rel={rel} 
     >
       {children}
     </Link>
