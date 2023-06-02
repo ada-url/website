161c7f24493beacd6a569c83e1ae3bfe3d869020
Yagiz Nizipli <yagiz@nizipli.com>
2023-06-02 14:33:54 -0400

    fix: handle mobile navigation on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -15,41 +15,38 @@ export const metadata: Metadata = {
 export default function IndexPage() {
   return (
     <main className='container relative flex flex-col mb-24'>
-      <div className='container flex max-w-[64rem] flex-col items-center gap-4 text-center'>
-        <section className='space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32'>
-          <div className='container flex max-w-[64rem] flex-col items-center gap-4 text-center'>
-            <Icons.logo className='h-36' />
-            <h1 className='font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
-              Meet Ada: WHATWG Compliant URL Parser
-            </h1>
-            <p className='max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8'>
-              Parse millions of URLs per second
-            </p>
-            <div className='space-x-4 flex flex-row'>
-              <Link href='/docs/introduction' className={cn(buttonVariants({ size: 'lg' }))}>
-                <Icons.link className='mr-2 h-4 w-4 hidden sm:visible' />
-                Get Started
-              </Link>
-              <Link
-                href={siteConfig.links.github}
-                target='_blank'
-                rel='noreferrer'
-                className={cn(buttonVariants({ variant: 'outline', size: 'lg' }))}
-              >
-                <Icons.gitHub className='mr-2 h-4 w-4 hidden sm:visible' />
-                GitHub
-              </Link>
-            </div>
+      <section className='space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32'>
+        <div className='container flex max-w-[64rem] flex-col items-center gap-4 text-center'>
+          <Icons.logo className='h-36' />
+          <h1 className='font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
+            Meet Ada: WHATWG Compliant URL Parser
+          </h1>
+          <p className='max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8'>
+            Parse millions of URLs per second
+          </p>
+          <div className='space-x-4 flex flex-row'>
+            <Link href='/docs/introduction' className={cn(buttonVariants({ size: 'lg' }))}>
+              <Icons.link className='mr-2 h-4 w-4 hidden sm:visible' />
+              Get Started
+            </Link>
             <Link
-              href='/playground'
-              className='rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium'
+              href={siteConfig.links.github}
               target='_blank'
+              rel='noreferrer'
+              className={cn(buttonVariants({ variant: 'outline', size: 'lg' }))}
             >
-              Try it on the playground
+              <Icons.gitHub className='mr-2 h-4 w-4 hidden sm:visible' />
+              GitHub
             </Link>
           </div>
-        </section>
-      </div>
+          <Link
+            href='/playground'
+            className='rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium'
+          >
+            Try it on the playground
+          </Link>
+        </div>
+      </section>
 
       <section className='container space-y-6 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24'>
         <div className='mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center'>
