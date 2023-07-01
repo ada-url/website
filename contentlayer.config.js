803830138a1557e34942beb4c3c7320e59f7d141
Yagiz Nizipli <yagiz@nizipli.com>
2023-07-01 16:05:45 -0400

    chore: update all dependencies

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -115,6 +115,7 @@ export default makeSource({
             }
           },
           onVisitHighlightedLine(node) {
+            node.properties.className ??= []
             node.properties.className.push('line--highlighted')
           },
           onVisitHighlightedWord(node) {
