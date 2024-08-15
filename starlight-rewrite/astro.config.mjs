f2992362ee7beb36a0e47fcfeb4acaac04ad83a2
nafees nazik <nafeesnazik21@gmail.com>
2024-08-15 20:28:28 +0530

    feat: add installation doc

--- starlight-rewrite/astro.config.mjs
+++ starlight-rewrite/astro.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig({
           items: [
             // Each item here is one entry in the navigation menu.
             { label: "Introduction", slug: "docs/introduction" },
+            { label: "Installation", slug: "docs/installation" },
           ],
         },
       ],
