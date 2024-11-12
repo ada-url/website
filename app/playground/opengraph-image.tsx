14b4d0bdba4b223b69dc6f8a8c7569c3773a9a87
Yagiz Nizipli <yagiz@cloudflare.com>
2024-11-12 12:14:38 -0500

    remove next.js project

--- app/playground/opengraph-image.tsx
+++
@@ -1,9 +0,0 @@
-import { SocialImage, imageSize } from '@/components/social-image'
-
-export const runtime = 'edge'
-export const contentType = 'image/png'
-export const size = imageSize
-
-export default async function Image() {
-  return SocialImage({ title: 'Ada URL Parser playground' })
-}
--- app/twitter-image.tsx
+++
@@ -1,8 +0,0 @@
-import { imageSize } from '@/components/social-image'
-import Image from './opengraph-image'
-
-export const runtime = 'edge'
-export const contentType = 'image/png'
-export const size = imageSize
-
-export default Image
